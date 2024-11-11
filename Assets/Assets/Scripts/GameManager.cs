445619526d9cce6b6a73a9a0ab0d0538390e8f8f
palzzpal <delaney.dion74@gmail.com>
2024-11-10 23:02:59 -0500

    Completion Week 12

---
+++ Assets/Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject enemy;
+    public GameObject cloud;
+    public GameObject coin;
+    private int score;
+    private int lives;
+
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI livesText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(player, transform.position, Quaternion.identity);
+        InvokeRepeating("CreateEnemy", 1f, 3f);
+        InvokeRepeating("CreateCoin", 3f, 5f);
+        CreateSky();
+        score = 0;
+        scoreText.text = "Score: " + score;
+        lives = 3;
+        livesText.text = "Lives: " + lives;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    void CreateEnemy()
+    {
+        Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 7.5f, 0), Quaternion.identity);
+    }
+
+    void CreateSky()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Instantiate(cloud, transform.position, Quaternion.identity);
+        }
+    }
+
+    void CreateCoin()
+    {
+        Instantiate(coin, new Vector3(Random.Range(-9f, 9f), 7.5f, 0), Quaternion.identity);
+    }
+
+    public void EarnScore(int howMuch)
+    {
+        score = score + howMuch;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void LoseLife()
+    {
+        lives--;
+        livesText.text = "Lives: " + lives;
+    }
+}
